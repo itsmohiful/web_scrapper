bccf8dfe1d9c1c81a9c87456266780c66d490e00
Soyad Mohiful Islam <mohiful.islam.ofc@gmail.com>
2022-06-03 01:30:59 +0600

    build scrapper function

--- scrapper/views.py
+++ scrapper/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .models import Link
+
+
+def scraper(request):
+    if request.method == "POST":
+        site = request.POST.get('site','')
+        page = requests.get(site)
+        soup = BeautifulSoup(page.text,'html.parser')
+
+        for link in soup.find_all('a'):
+            link_name  = link.string
+            link_address = link.get('href')
+
+            Link.objects.create(name=link_name,address=link_address)
+        return redirect('/')
+    
+    else:
+        data = Link.objects.all()
+
+    context = {
+        'data' : data
+    }
+    return render(request,'scraper/result.html',context)
